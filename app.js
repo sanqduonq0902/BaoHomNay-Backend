3d3711b5b175aee4ed1a2d39d625032e57faaf8b
SanqDuonq <namsang0902s@gmail.com>
2025-06-03 22:36:57 +0700

    Fix app

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+require('dotenv').config();
 
-const authRoutes = require('./routes/authRoutes');
-const articleRoutes = require('./routes/articleRoutes');
-const commentRoutes = require('./routes/commentRoutes');
-const userRoutes = require('./routes/userRoutes');
+const authRoutes = require("./routes/authRoutes");
+const articleRoutes = require("./routes/articleRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/articles', articleRoutes);
-app.use('/api/comments', commentRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/articles", articleRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+	res.send("Backend is running");
+});
+
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log("MongoDB connected");
+		app.listen(process.env.PORT || 5000, () => {
+			console.log(`Server running on port ${process.env.PORT}`);
+		});
+	})
+	.catch((err) => console.error("MongoDB connection error:", err));
 
 module.exports = app;
